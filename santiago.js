ae8bd7c19fd99847a22723b7638ac289005421d3
sgb06 <santigallego2706@gmail.com>
2023-08-01 19:56:54 -0500

    archivo js

---
+++ santiago.js
@@ -0,0 +1,8 @@
+let name = "santiago"
+let lastname = "gallego"
+let yearofbirth = "2001"
+let city = "medellin"
+
+console.log ("Hola, mi nombres es " ,name , lastname, "vivo en ", city, "y tengo 22 anios")
+
+
